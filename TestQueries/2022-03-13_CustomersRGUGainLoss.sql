f02aedde8651a3813bcef1cbd3cca05e8f26105a
paulamoreno96 <96207117+paulamoreno96@users.noreply.github.com>
2022-03-15 18:28:11 -0500

    Update 2022-03-13_CustomersRGUGainLoss.sql

--- TestQueries/2022-03-13_CustomersRGUGainLoss.sql
+++ TestQueries/2022-03-13_CustomersRGUGainLoss.sql
@@ -5,7 +5,7 @@ WHEN pd_mix_cd = "2P" THEN 2
 WHEN pd_mix_cd = "3P" THEN 3
 ELSE NULL END AS NUMRGUS1
 FROM `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.cwc_jam_dna_fullmonth_202202` 
-WHERE org_cntry = "Jamaica" AND load_dt = '2022-02-01' 
+WHERE org_cntry = "Jamaica" AND load_dt = '2022-02-01' AND (fi_outst_age < 90 OR fi_outst_age IS NULL)
 GROUP BY act_acct_cd, NUMRGUS1
 ),
 RGUSULTIMODIA AS(
@@ -15,7 +15,7 @@ WHEN pd_mix_cd = "2P" THEN 2
 WHEN pd_mix_cd = "3P" THEN 3
 ELSE NULL END AS NUMRGUS28
 FROM `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.cwc_jam_dna_fullmonth_202202` 
-WHERE org_cntry = "Jamaica" AND load_dt = '2022-02-28' 
+WHERE org_cntry = "Jamaica" AND load_dt = '2022-02-28' --AND (fi_outst_age < 90 OR fi_outst_age IS NULL)
 GROUP BY act_acct_cd, NUMRGUS28
 ),
 CAMBIORGUS AS
@@ -27,6 +27,6 @@ CAMBIORGUS AS
     ELSE NULL END AS CAMBIORGUSMES
     FROM RGUSPRIMERDIA p LEFT JOIN RGUSULTIMODIA u ON p.act_acct_cd = u.act_acct_cd
 )
-SELECT CAMBIORGUSMES, COUNT (DISTINCT act_acct_cd)
+SELECT CAMBIORGUSMES, COUNT (DISTINCT act_acct_cd) as NumUsers
 FROM CAMBIORGUS
 GROUP BY CAMBIORGUSMES
