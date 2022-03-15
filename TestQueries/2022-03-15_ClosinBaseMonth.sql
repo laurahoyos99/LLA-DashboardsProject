1bd94587ad815dcf5d479e01fce598600a91170a
paulamoreno96 <96207117+paulamoreno96@users.noreply.github.com>
2022-03-15 15:22:45 -0500

    Create 2022-03-15_ClosinBaseMonth.sql

---
+++ TestQueries/2022-03-15_ClosinBaseMonth.sql
@@ -0,0 +1,17 @@
+WITH MONTHLASTDAY AS(
+ SELECT DATE_TRUNC (LOAD_DT, MONTH) AS MES, MAX(LOAD_DT) AS LASTDAY
+ FROM `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.cwc_jam_dna_fullmonth_202202` 
+ WHERE org_cntry = "Jamaica" 
+ GROUP BY MES
+),
+CLOSINGBASE AS(
+SELECT DISTINCT MES, LASTDAY, act_acct_cd
+FROM `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.cwc_jam_dna_fullmonth_202202` t
+INNER JOIN MONTHLASTDAY l ON l.LASTDAY = t.load_dt AND DATE_TRUNC (t.LOAD_DT, MONTH) = l.MES
+WHERE org_cntry = "Jamaica"
+GROUP BY MES, act_acct_cd, LASTDAY
+)
+SELECT MES, LASTDAY, COUNT (DISTINCT act_acct_cd) AS CLOSINGBASECUSTOMERS
+FROM CLOSINGBASE
+GROUP BY MES,LASTDAY
+ORDER BY MES, LASTDAY
