c165e125dfa2df6deaa877b681c097237dcc9102
paulamoreno96 <96207117+paulamoreno96@users.noreply.github.com>
2022-03-15 18:44:58 -0500

    Update 2022-03-15_DailyCustomersBase.sql

--- TestQueries/2022-03-15_DailyCustomersBase.sql
+++ TestQueries/2022-03-15_DailyCustomersBase.sql
@@ -1,14 +1,14 @@
 WITH MONTHLASTDAY AS(
  SELECT DATE_TRUNC (LOAD_DT, MONTH) AS MES, LOAD_DT AS MONTHDATE
  FROM `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.cwc_jam_dna_fullmonth_202202` 
- WHERE org_cntry = "Jamaica" --AND LOAD_DT = '2022-02-25' 
+ WHERE org_cntry = "Jamaica" 
  GROUP BY MES, load_dt
 ),
 CLOSINGBASE AS(
 SELECT DISTINCT MES, MONTHDATE, act_acct_cd
 FROM `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.cwc_jam_dna_fullmonth_202202` t
 INNER JOIN MONTHLASTDAY l ON l.MONTHDATE = t.load_dt AND DATE_TRUNC (t.LOAD_DT, MONTH) = l.MES
-WHERE org_cntry = "Jamaica" --AND load_dt = '2022-02-28' 
+WHERE org_cntry = "Jamaica" AND (fi_outst_age < 90 OR fi_outst_age IS NULL)
 GROUP BY MES, act_acct_cd, MONTHDATE
 )
 SELECT MES, MONTHDATE, COUNT (DISTINCT act_acct_cd) AS CLOSINGBASECUSTOMERS
