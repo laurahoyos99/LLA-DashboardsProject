5742809b0ea9daf73581ec3a7bc118e627bf3116
paulamoreno96 <96207117+paulamoreno96@users.noreply.github.com>
2022-03-15 12:27:28 -0500

    Update 2022-03-13_CustomersRGUGain.sql

--- TestQueries/2022-03-13_CustomersRGUGain.sql
+++ TestQueries/2022-03-13_CustomersRGUGain.sql
@@ -1,31 +1,32 @@
 WITH RGUSPRIMERDIA AS(
-SELECT DISTINCT CST_CUST_CD, 
+SELECT DISTINCT act_acct_cd, 
 CASE WHEN pd_mix_cd = "1P" THEN 1
 WHEN pd_mix_cd = "2P" THEN 2
 WHEN pd_mix_cd = "3P" THEN 3
 ELSE NULL END AS NUMRGUS1
 FROM `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.cwc_jam_dna_fullmonth_202202` 
 WHERE org_cntry = "Jamaica" AND load_dt = '2022-02-01' 
-GROUP BY cst_cust_cd, pd_mix_cd
+GROUP BY act_acct_cd, NUMRGUS1
 ),
 RGUSULTIMODIA AS(
-SELECT DISTINCT CST_CUST_CD, 
+SELECT DISTINCT act_acct_cd, 
 CASE WHEN pd_mix_cd = "1P" THEN 1
 WHEN pd_mix_cd = "2P" THEN 2
 WHEN pd_mix_cd = "3P" THEN 3
 ELSE NULL END AS NUMRGUS28
 FROM `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.cwc_jam_dna_fullmonth_202202` 
 WHERE org_cntry = "Jamaica" AND load_dt = '2022-02-28' 
+GROUP BY act_acct_cd, NUMRGUS28
 ),
 CAMBIORGUS AS
 (
-    SELECT DISTINCT p.CST_CUST_CD,
+    SELECT DISTINCT p.act_acct_cd,
     CASE WHEN (NUMRGUS28 - NUMRGUS1) > 0 THEN "Gain"
     WHEN (NUMRGUS28 - NUMRGUS1) < 0 THEN "Loss"
     WHEN (NUMRGUS28 - NUMRGUS1) = 0 THEN "Maintain"
     ELSE NULL END AS CAMBIORGUSMES
-    FROM RGUSPRIMERDIA p LEFT JOIN RGUSULTIMODIA u ON p.cst_cust_cd = u.cst_cust_cd
+    FROM RGUSPRIMERDIA p LEFT JOIN RGUSULTIMODIA u ON p.act_acct_cd = u.act_acct_cd
 )
-SELECT CAMBIORGUSMES, COUNT (DISTINCT CST_CUST_CD)
+SELECT CAMBIORGUSMES, COUNT (DISTINCT act_acct_cd)
 FROM CAMBIORGUS
 GROUP BY CAMBIORGUSMES
