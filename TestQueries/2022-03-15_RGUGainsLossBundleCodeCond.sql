925ca1ba795764aae16d015376f8559ac7cb5c41
paulamoreno96 <96207117+paulamoreno96@users.noreply.github.com>
2022-03-15 14:14:52 -0500

    Create 2022-03-15_RGUGainsLossBundleCodeCond.sql

---
+++ TestQueries/2022-03-15_RGUGainsLossBundleCodeCond.sql
@@ -0,0 +1,32 @@
+WITH RGUSPRIMERDIA AS(
+SELECT DISTINCT act_acct_cd, 
+CASE WHEN pd_mix_cd = "1P" THEN 1
+WHEN pd_mix_cd = "2P" OR (pd_mix_cd = "3P" AND bundle_code ="GPON_HBO_GO") THEN 2
+WHEN pd_mix_cd = "3P" AND (bundle_code <> "GPON_HBO_GO") THEN 3
+ELSE NULL END AS NUMRGUS1
+FROM `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.cwc_jam_dna_fullmonth_202202` 
+WHERE org_cntry = "Jamaica" AND load_dt = '2022-02-01' 
+GROUP BY act_acct_cd, NUMRGUS1
+),
+RGUSULTIMODIA AS(
+SELECT DISTINCT act_acct_cd, 
+CASE WHEN pd_mix_cd = "1P" THEN 1
+WHEN pd_mix_cd = "2P" OR (pd_mix_cd = "3P" AND bundle_code ="GPON_HBO_GO") THEN 2
+WHEN pd_mix_cd = "3P" AND (bundle_code <> "GPON_HBO_GO") THEN 3
+ELSE NULL END AS NUMRGUS28
+FROM `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.cwc_jam_dna_fullmonth_202202` 
+WHERE org_cntry = "Jamaica" AND load_dt = '2022-02-28' 
+GROUP BY act_acct_cd, NUMRGUS28
+),
+CAMBIORGUS AS
+(
+    SELECT DISTINCT p.act_acct_cd,
+    CASE WHEN (NUMRGUS28 - NUMRGUS1) > 0 THEN "Gain"
+    WHEN (NUMRGUS28 - NUMRGUS1) < 0 THEN "Loss"
+    WHEN (NUMRGUS28 - NUMRGUS1) = 0 THEN "Maintain"
+    ELSE NULL END AS CAMBIORGUSMES
+    FROM RGUSPRIMERDIA p LEFT JOIN RGUSULTIMODIA u ON p.act_acct_cd = u.act_acct_cd
+)
+SELECT CAMBIORGUSMES, COUNT (DISTINCT act_acct_cd)
+FROM CAMBIORGUS
+GROUP BY CAMBIORGUSMES
