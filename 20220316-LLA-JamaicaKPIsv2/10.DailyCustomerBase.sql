6ebe4fcf7e506536f02082113f4f2ee92438b49c
paulamoreno96 <96207117+paulamoreno96@users.noreply.github.com>
2022-03-16 11:18:35 -0500

    Update 10.DailyCustomerBase.sql

--- 20220316-LLA-JamaicaKPIsv2/10.DailyCustomerBase.sql
+++ 20220316-LLA-JamaicaKPIsv2/10.DailyCustomerBase.sql
@@ -1,4 +1,4 @@
-WITH MONTHLASTDAY AS(
+WITH MONTHDATES AS(
  SELECT DATE_TRUNC (LOAD_DT, MONTH) AS MES, LOAD_DT AS MONTHDATE
  FROM `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.cwc_jam_dna_fullmonth_202202` 
  WHERE org_cntry = "Jamaica" 
@@ -7,7 +7,7 @@ WITH MONTHLASTDAY AS(
 CLOSINGBASE AS(
 SELECT DISTINCT MES, MONTHDATE, act_acct_cd
 FROM `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.cwc_jam_dna_fullmonth_202202` t
-INNER JOIN MONTHLASTDAY l ON l.MONTHDATE = t.load_dt AND DATE_TRUNC (t.LOAD_DT, MONTH) = l.MES
+INNER JOIN MONTHDATES l ON l.MONTHDATE = t.load_dt AND DATE_TRUNC (t.LOAD_DT, MONTH) = l.MES
 WHERE org_cntry = "Jamaica" AND (fi_outst_age < 90 OR fi_outst_age IS NULL) AND
 ACT_CUST_TYP_NM IN ('Browse & Talk HFONE', 'Residence', 'Standard') AND ACT_ACCT_STAT IN ('B','D','P','SN','SR','T','W')
 GROUP BY MES, act_acct_cd, MONTHDATE
